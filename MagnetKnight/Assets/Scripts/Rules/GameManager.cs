00c2db95baed393bc0bb2fe553418cc680879bbc
antronik7 <antoineberthiaumedutil@gmail.com>
2018-06-23 23:43:02 -0400

    Created the Game Manager

---
+++ MagnetKnight/Assets/Scripts/Rules/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null) 
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
